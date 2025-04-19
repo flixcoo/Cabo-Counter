59ddb7dce64d2d1f3a57f8bbd681474b8d13bbd2
Felix Kirchner <felix.kirchner@stud.htwk-leipzig.de>
2025-04-19 10:34:37 +0200

    Removed app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
-import 'package:cabo_counter/app.dart';
+import 'package:cabo_counter/utility/styles.dart';
+import 'package:cabo_counter/views/main_menu_view.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      theme: CupertinoThemeData(
+          primaryColor: Styles.primaryColor,
+          textTheme: CupertinoTextThemeData(
+            primaryColor: Styles.primaryColor,
+          )),
+      home: MainMenuView(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
