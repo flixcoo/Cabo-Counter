b72c452f9f55c2588fddf40fe2b092debfc155b6
Felix Kirchner <felix.kirchner@stud.htwk-leipzig.de>
2025-04-23 21:37:20 +0200

    Implemented full version of info view

--- lib/views/information_view.dart
+++ lib/views/information_view.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InformationView extends StatelessWidget {
   const InformationView({super.key});
+
+  Future<PackageInfo> _getPackageInfo() async {
+    return await PackageInfo.fromPlatform();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,38 +18,83 @@ class InformationView extends StatelessWidget {
           middle: Text('Über'),
         ),
         child: SafeArea(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+            child: Stack(
           children: [
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 20.0),
-              child: Text(
-                'Cabo Counter',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20.0),
+                  child: Text(
+                    'Cabo Counter',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
+                Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Text(
+                      'Hey :)\nDanke, dass du als eine:r der ersten User '
+                      'meiner ersten eigenen App dabei bist! Ich hab sehr '
+                      'viel Arbeit in dieses Projekt gesteckt und auch, '
+                      'wenn ich (hoffentlich) an vieles Gedacht hab, wird '
+                      'auf jeden Fall noch nicht alles 100% funktionieren. '
+                      'Solltest du also irgendwelche Fehler entdecken oder '
+                      'Feedback zum Design oder der Benutzerfreundlichekeit'
+                      ' haben, zögere bitte nicht sie mir auf den dir '
+                      'bekannten Wegen mitzuteilen. Danke! ',
+                      textAlign: TextAlign.center,
+                    )),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                        onPressed: () => launchUrl(
+                            Uri.parse('https://www.instagram.com/fx.kr')),
+                        icon: Icon(FontAwesomeIcons.instagram)),
+                    IconButton(
+                        onPressed: () => launchUrl(
+                            Uri.parse('mailto:felix.kirchner.fk@gmail.com')),
+                        icon: Icon(CupertinoIcons.envelope)),
+                    IconButton(
+                        onPressed: () => launchUrl(
+                            Uri.parse('https://www.github.com/flixcoo')),
+                        icon: Icon(FontAwesomeIcons.github)),
+                  ],
+                )
+              ],
+            ),
+            Positioned(
+              bottom: 30,
+              left: 0,
+              right: 0,
+              child: FutureBuilder<PackageInfo>(
+                future: _getPackageInfo(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(
+                      'App-Version ${snapshot.data!.version} '
+                      '(Build ${snapshot.data!.buildNumber})',
+                      textAlign: TextAlign.center,
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text(
+                      'App-Version -.-.- (Build -)',
+                      textAlign: TextAlign.center,
+                    );
+                  }
+                  return Text(
+                    'Lade Version...',
+                    textAlign: TextAlign.center,
+                  );
+                },
               ),
             ),
-            Text(
-              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed '
-              'diam nonumy eirmod tempor invidunt ut labore et dolore '
-              'magna aliquyam erat, sed diam voluptua. At vero eos et '
-              'accusam et justo duo dolores et ea rebum. Stet clita kasd '
-              'gubergren, no sea takimata sanctus est Lorem ipsum dolor '
-              'sit amet. Lorem ipsum dolor sit amet, consetetur '
-              'sadipscing elitr, sed diam nonumy eirmod tempor invidunt '
-              'ut labore et dolore magna aliquyam erat, sed diam '
-              'voluptua. At vero eos et accusam et justo duo dolores et '
-              'ea rebum. Stet clita kasd gubergren, no sea takimata '
-              'sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor '
-              'sit amet, consetetur sadipscing elitr, sed diam nonumy '
-              'eirmod tempor invidunt ut labore et dolore magna aliquyam '
-              'erat, sed diam voluptua. At vero eos et accusam et justo '
-              'duo dolores et ea rebum. Stet clita kasd gubergren, no '
-              'sea takimata sanctus est Lorem ipsum dolor sit amet. ',
-              textAlign: TextAlign.center,
-            )
           ],
         )));
   }
