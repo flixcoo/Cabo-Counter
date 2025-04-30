3cf8e91905a9c7df944506744a7ea5a3429b565a
Felix Kirchner <felix.kirchner@stud.htwk-leipzig.de>
2025-04-30 14:15:08 +0200

    Updates global list for storing games

--- lib/utility/globals.dart
+++ lib/utility/globals.dart
@@ -1,5 +1,11 @@
 import 'package:cabo_counter/data/game_session.dart';
 
 class Globals {
-  static Map<int, GameSession> gamesMap = <int, GameSession>{};
+  /// The [gameList] contains all active game sessions.
+  static List<GameSession> gameList = [];
+
+  static void addGameSession(GameSession session) {
+    gameList.add(session);
+    gameList.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+  }
 }
