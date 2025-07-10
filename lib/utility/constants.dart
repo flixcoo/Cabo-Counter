ef2c9065155ecf79e74b47aa8607639d15deb57a
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-07-10 17:15:38 +0200

    Corrected wiki link

--- lib/utility/constants.dart
+++ lib/utility/constants.dart
@@ -8,7 +8,7 @@ class Constants {
   static const String GITHUB_ISSUES_LINK =
       'https://github.com/flixcoo/Cabo-Counter/issues';
   static const String GITHUB_WIKI_LINK =
-      'https://github.com/flixcoo/Cabo-Counter/issues';
+      'https://github.com/flixcoo/Cabo-Counter/wiki';
   static const String EMAIL = 'cabocounter@felixkirchner.de';
   static const String PRIVACY_POLICY_LINK =
       'https://www.privacypolicies.com/live/1b3759d4-b2f1-4511-8e3b-21bb1626be68';
