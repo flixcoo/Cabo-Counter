cc6a7fee2543d3c4b6382816984c2e5866478dec
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-07-16 14:27:38 +0200

    Fixed bug with wrong medal icon

--- lib/presentation/views/round_view.dart
+++ lib/presentation/views/round_view.dart
@@ -197,11 +197,13 @@ class _RoundViewState extends State<RoundView> {
                                     overflow: TextOverflow.ellipsis,
                                   ),
                                   Visibility(
-                                    visible: index == 0,
+                                    visible:
+                                        index == 0 && widget.roundNumber != 1,
                                     child: const SizedBox(width: 10),
                                   ),
                                   Visibility(
-                                      visible: index == 0,
+                                      visible:
+                                          index == 0 && widget.roundNumber != 1,
                                       child: const Icon(FontAwesomeIcons.medal,
                                           size: 15))
                                 ]))
