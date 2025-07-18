5b418817b20c9078fc95c501bded0c38f9d66f25
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-07-18 23:41:09 +0200

    Visual improvements on table

--- lib/presentation/views/point_overview_view.dart
+++ lib/presentation/views/point_overview_view.dart
@@ -1,3 +1,4 @@
+import 'package:cabo_counter/core/custom_theme.dart';
 import 'package:cabo_counter/data/game_session.dart';
 import 'package:cabo_counter/l10n/generated/app_localizations.dart';
 import 'package:flutter/cupertino.dart';
@@ -24,16 +25,32 @@ class _PointOverviewViewState extends State<PointOverviewView> {
         child: SingleChildScrollView(
             padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: DataTable(
                 dataRowMinHeight: 60,
                 dataRowMaxHeight: 60,
                 dividerThickness: 0.5,
                 columnSpacing: 20,
                 columns: [
-                  const DataColumn(label: Text('#')),
+                  const DataColumn(
+                      numeric: true,
+                      headingRowAlignment: MainAxisAlignment.center,
+                      label: Text(
+                        '#',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      columnWidth: IntrinsicColumnWidth(flex: 0.5)),
                   ...widget.gameSession.players.map(
-                    (player) => DataColumn(label: Text(player)),
+                    (player) => DataColumn(
+                        label: FittedBox(
+                            fit: BoxFit.fill,
+                            child: Text(
+                              player,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            )),
+                        headingRowAlignment: MainAxisAlignment.center,
+                        columnWidth: const IntrinsicColumnWidth(flex: 1)),
                   ),
                 ],
                 rows: List<DataRow>.generate(
@@ -46,40 +63,47 @@ class _PointOverviewViewState extends State<PointOverviewView> {
                           alignment: Alignment.center,
                           child: Text(
                             '$roundIndex',
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
+                            style: const TextStyle(fontSize: 20),
                           ),
                         )),
                         ...List.generate(widget.gameSession.players.length,
                             (playerIndex) {
                           final score = round.scores[playerIndex];
                           final update = round.scoreUpdates[playerIndex];
+                          final saidCabo = round.caboPlayerIndex == playerIndex
+                              ? true
+                              : false;
                           return DataCell(
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 6, vertical: 2),
-                                  decoration: BoxDecoration(
-                                    color: update <= 0
-                                        ? Colors.green[200]
-                                        : Colors.red[200],
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  child: Text(
-                                    '${update >= 0 ? '+' : '-'}$update',
-                                    style: TextStyle(
+                            Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 6, vertical: 2),
+                                    decoration: BoxDecoration(
                                       color: update <= 0
-                                          ? Colors.green[900]
-                                          : Colors.red[900],
-                                      fontWeight: FontWeight.bold,
+                                          ? CustomTheme.primaryColor
+                                          : CupertinoColors.destructiveRed,
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: Text(
+                                      '${update >= 0 ? '+' : '-'}$update',
+                                      style: const TextStyle(
+                                        color: CupertinoColors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text('$score'),
-                              ],
+                                  const SizedBox(height: 4),
+                                  Text('$score',
+                                      style: TextStyle(
+                                        fontWeight: saidCabo
+                                            ? FontWeight.bold
+                                            : FontWeight.normal,
+                                      )),
+                                ],
+                              ),
                             ),
                           );
                         }),
