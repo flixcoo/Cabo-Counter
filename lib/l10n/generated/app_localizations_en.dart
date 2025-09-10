8aa50b86cd6a44ce75d44089f2266c6944300f6a
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-09-10 23:23:38 +0200

    Implemented active filter sign

--- lib/l10n/generated/app_localizations_en.dart
+++ lib/l10n/generated/app_localizations_en.dart
@@ -234,11 +234,7 @@ class AppLocalizationsEn extends AppLocalizations {
 
   @override
   String bonus_points_message(
-    int playerCount,
-    String names,
-    int pointLimit,
-    int bonusPoints,
-  ) {
+      int playerCount, String names, int pointLimit, int bonusPoints) {
     String _temp0 = intl.Intl.pluralLogic(
       playerCount,
       locale: localeName,
