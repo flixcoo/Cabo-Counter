1c8422341167d10abf1bb0b0fd1cae1acaf77681
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-09-10 23:12:17 +0200

    Corrected comment

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -35,7 +35,7 @@ class Constants {
   /// URL to the app's imprint page, containing legal information.
   static const String kLegalLink = 'https://felixkirchner.de/impressum';
 
-  /// URL to the app's imprint page, containing legal information.
+  /// URL to my PayPal donation page for users who wish to support the app.
   static const String kDonateLink = 'https://www.paypal.me/flixcoo';
 
   /// Instance of [RateMyApp] configured to prompt users for app store ratings.
