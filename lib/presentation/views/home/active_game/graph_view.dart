0a8172a8af6ad7de44a391135cabcb211a4e4f48
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-10-05 19:26:20 +0200

    Implemented disabled button

--- lib/presentation/views/home/active_game/graph_view.dart
+++ lib/presentation/views/home/active_game/graph_view.dart
@@ -38,11 +38,13 @@ class _GraphViewState extends State<GraphView> {
 
   @override
   Widget build(BuildContext context) {
+    bool isGraphAvailable =
+        widget.gameSession.roundNumber > 1 || widget.gameSession.isGameFinished;
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
           middle: Text(AppLocalizations.of(context).scoring_history),
           trailing: IconButton(
-            onPressed: () => _shareImage(),
+            onPressed: () => isGraphAvailable ? _shareImage() : null,
             icon: const Icon(CupertinoIcons.share),
             iconSize: Constants.navBarIconSize,
           ),
@@ -50,8 +52,7 @@ class _GraphViewState extends State<GraphView> {
         ),
         child: SafeArea(
           child: Visibility(
-            visible: widget.gameSession.roundNumber > 1 ||
-                widget.gameSession.isGameFinished,
+            visible: isGraphAvailable,
             replacement: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
