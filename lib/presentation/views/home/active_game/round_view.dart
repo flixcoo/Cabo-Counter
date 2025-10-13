9eafefda1871f2c10d8b74a6b7ef6937ecf9669c
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-10-13 19:36:11 +0200

    Implemented kamikaze sheet class

--- lib/presentation/views/home/active_game/round_view.dart
+++ lib/presentation/views/home/active_game/round_view.dart
@@ -1,13 +1,11 @@
-import 'dart:io';
-
 import 'package:cabo_counter/core/custom_theme.dart';
 import 'package:cabo_counter/data/dto/game_session.dart';
 import 'package:cabo_counter/l10n/generated/app_localizations.dart';
 import 'package:cabo_counter/presentation/components/widgets/custom_button.dart';
+import 'package:cabo_counter/presentation/components/widgets/kamikaze_sheet.dart';
 import 'package:cabo_counter/services/icon_service.dart';
 import 'package:cabo_counter/services/popup_service.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -346,107 +344,15 @@ class _RoundViewState extends State<RoundView> {
     ];
   }
 
-  /// Shows a Cupertino action sheet to select the player who has Kamikaze.
+  /// Shows a bottom sheet sheet to select the player who has Kamikaze.
   /// It returns true if a player was selected, false if the action was cancelled.
   Future<bool> _showKamikazeSheet(BuildContext context) async {
-    if (Platform.isIOS) {
-      return await showCupertinoModalPopup<bool?>(
-            context: context,
-            builder: (BuildContext context) {
-              return CupertinoActionSheet(
-                title: Text(AppLocalizations.of(context).kamikaze),
-                message: Text(AppLocalizations.of(context).who_has_kamikaze),
-                actions:
-                    widget.gameSession.players.asMap().entries.map((entry) {
-                  final index = entry.key;
-                  final player = entry.value;
-                  return CupertinoActionSheetAction(
-                    onPressed: () {
-                      _kamikazePlayerIndex = index;
-                      Navigator.pop(context, true);
-                    },
-                    child: Text(
-                      player.name,
-                      style: TextStyle(color: CustomTheme.kamikazeColor),
-                    ),
-                  );
-                }).toList(),
-                cancelButton: CupertinoActionSheetAction(
-                  onPressed: () => Navigator.pop(context, false),
-                  isDestructiveAction: true,
-                  child: Text(AppLocalizations.of(context).cancel),
-                ),
-              );
-            },
-          ) ??
-          false;
-    } else {
-      return await showModalBottomSheet<bool?>(
-            context: context,
-            isDismissible: true,
-            isScrollControlled: true,
-            showDragHandle: true,
-            backgroundColor: CustomTheme.mainElementBackgroundColor,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-            ),
-            builder: (BuildContext context) {
-              return SafeArea(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      AppLocalizations.of(context).kamikaze,
-                      style: Theme.of(context).textTheme.titleLarge,
-                      textAlign: TextAlign.center,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8),
-                      child: Text(
-                        AppLocalizations.of(context).who_has_kamikaze,
-                        style: Theme.of(context).textTheme.bodyMedium,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    const Divider(
-                      indent: 40,
-                      endIndent: 40,
-                    ),
-                    ...widget.gameSession.players.asMap().entries.map((entry) {
-                      final index = entry.key;
-                      final player = entry.value;
-                      return Center(
-                        child: ListTile(
-                          title: Text(
-                            player.name,
-                            style: TextStyle(
-                                color: CustomTheme.kamikazeColor, fontSize: 18),
-                            textAlign: TextAlign.center,
-                          ),
-                          onTap: () {
-                            _kamikazePlayerIndex = index;
-                            Navigator.pop(context, true);
-                          },
-                        ),
-                      );
-                    }),
-                    ListTile(
-                      title: Text(
-                        AppLocalizations.of(context).cancel,
-                        style: TextStyle(color: CustomTheme.red, fontSize: 18),
-                        textAlign: TextAlign.center,
-                      ),
-                      onTap: () => Navigator.pop(context, false),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ) ??
-          false;
+    final selectedIndex = await KamikazeSheet.show(context, gameSession);
+    if (selectedIndex != null) {
+      _kamikazePlayerIndex = selectedIndex;
+      return true;
     }
+    return false;
   }
 
   /// Focuses the next text field in the list of text fields.
