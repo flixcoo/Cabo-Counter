8911037e566cfdc7da9d465469f6a4bb5e639b55
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-12-02 23:38:56 +0100

    Added doc

--- lib/services/icon_service.dart
+++ lib/services/icon_service.dart
@@ -135,6 +135,7 @@ abstract class IconService {
       ? CupertinoIcons.eye_slash_fill
       : Icons.visibility_off_rounded;
 
+  /// Icon for shuffle rotation.
   static IconData get shuffle_cards => Platform.isIOS
       ? CupertinoIcons.rectangle_fill_on_rectangle_angled_fill
       : Icons.casino;
