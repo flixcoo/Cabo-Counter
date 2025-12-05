3946a87016163a1a3ec353fbf85067c0ddcc1406
Felix Kirchner <felix.kirchner.fk@gmail.com>
2025-12-05 14:03:42 +0100

    Added basic database test setup

---
+++ test/db_tests/game_session_test.dart
@@ -0,0 +1,21 @@
+import 'package:cabo_counter/data/db/database.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late AppDatabase database;
+
+  setUp(() {
+    database = AppDatabase(
+      DatabaseConnection(
+        NativeDatabase.memory(),
+        // Recommended for widget tests to avoid test errors.
+        closeStreamsSynchronously: true,
+      ),
+    );
+  });
+  tearDown(() async {
+    await database.close();
+  });
+}
