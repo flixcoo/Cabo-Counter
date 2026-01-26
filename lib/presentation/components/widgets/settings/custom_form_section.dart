ca82d039284d59ccf9c7fef7f550ea485830a224
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-01-26 17:02:49 +0100

    Added custom form section

---
+++ lib/presentation/components/widgets/settings/custom_form_section.dart
@@ -0,0 +1,52 @@
+import 'package:cabo_counter/core/custom_theme.dart';
+import 'package:cabo_counter/presentation/components/widgets/settings/custom_form_row.dart';
+import 'package:flutter/material.dart';
+
+class CustomFormSection extends StatelessWidget {
+  const CustomFormSection({super.key, required this.rows, this.infoText});
+
+  final List<CustomFormRow> rows;
+  final String? infoText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 15, 10, 0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: CustomTheme.settingsTilecolor,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (int i = 0; i < rows.length; i++) ...[
+              rows[i],
+              if (i < rows.length - 1)
+                Divider(
+                  height: 1,
+                  thickness: 1.5,
+                  color: CustomTheme.white.withAlpha(30),
+                  indent: 16,
+                  endIndent: 16,
+                ),
+            ],
+            if (infoText != null) ...[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12, left: 12, right: 12),
+                child: Text(
+                  infoText!,
+                  style: TextStyle(
+                    color: CustomTheme.white.withAlpha(150),
+                    fontSize: 13,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
