7aa601796336c17492475894775f1dd22bbbbc99
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-01-27 19:54:36 +0100

    Updated game tile design

--- lib/presentation/components/widgets/game_tile.dart
+++ lib/presentation/components/widgets/game_tile.dart
@@ -16,79 +16,86 @@ class GameTile extends StatefulWidget {
 class _GameTileState extends State<GameTile> {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          CupertinoPageRoute(
-            builder: (context) => ActiveGameView(gameSession: widget.session),
-          ),
-        );
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Container(
-                  alignment: AlignmentGeometry.center,
-                  width: 45,
-                  height: 45,
-                  decoration: BoxDecoration(
-                    color: CustomTheme.primaryColor.withAlpha(100),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 6),
+      decoration: BoxDecoration(
+        color: CustomTheme.mainElementBackgroundColor,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            CupertinoPageRoute(
+              builder: (context) => ActiveGameView(gameSession: widget.session),
+            ),
+          );
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    alignment: AlignmentGeometry.center,
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: CustomTheme.primaryColor.withAlpha(100),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
 
-                  child: widget.session.isPointsLimitEnabled
-                      ? Text(
-                          widget.session.pointLimit.toString(),
-                          style: TextStyle(
+                    child: widget.session.isPointsLimitEnabled
+                        ? Text(
+                            widget.session.pointLimit.toString(),
+                            style: TextStyle(
+                              color: CustomTheme.primaryColor.withRed(40),
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        : Icon(
+                            CupertinoIcons.arrow_2_circlepath,
+                            size: 32,
                             color: CustomTheme.primaryColor.withRed(40),
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
                           ),
-                        )
-                      : Icon(
-                          CupertinoIcons.arrow_2_circlepath,
-                          size: 32,
-                          color: CustomTheme.primaryColor.withRed(40),
-                        ),
-                ),
-                const SizedBox(width: 15),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.session.gameTitle,
-                      style: const TextStyle(fontSize: 16),
-                    ),
-                    Text(
-                      widget.session.isGameFinished
-                          ? '\u{1F947} ${widget.session.winner}'
-                          : 'In Progress',
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: CupertinoColors.systemGrey,
+                  ),
+                  const SizedBox(width: 15),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.session.gameTitle,
+                        style: const TextStyle(fontSize: 16),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  '${widget.session.players.length}',
-                  style: const TextStyle(fontSize: 16),
-                ),
-                const SizedBox(width: 10),
-                Icon(IconService.players),
-              ],
-            ),
-          ],
+                      Text(
+                        widget.session.isGameFinished
+                            ? '\u{1F947} ${widget.session.winner}'
+                            : 'In Progress',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: CupertinoColors.systemGrey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${widget.session.players.length}',
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(width: 10),
+                  Icon(IconService.players),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
