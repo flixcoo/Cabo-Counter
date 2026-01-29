7b4ae7813e512d6ce801c77fb6c483b08d6a8a04
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-01-30 00:22:58 +0100

    Removed dead code

--- lib/presentation/views/home/create_game/mode_selection_view.dart
+++ lib/presentation/views/home/create_game/mode_selection_view.dart
@@ -1,3 +1,4 @@
+import 'package:cabo_counter/core/enums.dart';
 import 'package:cabo_counter/l10n/generated/app_localizations.dart';
 import 'package:cabo_counter/presentation/components/widgets/mode_tile.dart';
 import 'package:flutter/cupertino.dart';
@@ -31,64 +32,22 @@ class ModeSelectionMenu extends StatelessWidget {
           ModeTile(
             title: '$pointLimit ${loc.points}',
             description: loc.point_limit_description(pointLimit),
+            onTap: () {
+              Navigator.pop(context, GameMode.pointLimit);
+            },
           ),
           ModeTile(
             title: loc.unlimited,
             description: loc.unlimited_description,
+            onTap: () {
+              Navigator.pop(context, GameMode.unlimited);
+            },
           ),
           if (showDeselection)
             ModeTile(
               title: loc.no_default_mode,
               description: loc.no_default_description,
             ),
-          /*Padding(
-            padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-            child: CupertinoListTile(
-              title: Text(
-                '$pointLimit ${loc.points}',
-                style: CustomTheme.modeTitle,
-              ),
-              subtitle: Text(
-                loc.point_limit_description(pointLimit),
-                style: CustomTheme.modeDescription,
-                maxLines: 3,
-              ),
-              onTap: () {
-                Navigator.pop(context, GameMode.pointLimit);
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-            child: CupertinoListTile(
-              title: Text(loc.unlimited, style: CustomTheme.modeTitle),
-              subtitle: Text(
-                loc.unlimited_description,
-                style: CustomTheme.modeDescription,
-                maxLines: 3,
-              ),
-              onTap: () {
-                Navigator.pop(context, GameMode.unlimited);
-              },
-            ),
-          ),
-          Visibility(
-            visible: showDeselection,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
-              child: CupertinoListTile(
-                title: Text(loc.no_default_mode, style: CustomTheme.modeTitle),
-                subtitle: Text(
-                  loc.no_default_description,
-                  style: CustomTheme.modeDescription,
-                  maxLines: 3,
-                ),
-                onTap: () {
-                  Navigator.pop(context, GameMode.none);
-                },
-              ),
-            ),
-          ),*/
         ],
       ),
     );
