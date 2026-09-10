24d17347679c9a730fc2e1524e4bc31234394ab4
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-09-10 20:38:21 +0200

    Added evaluation view news

--- lib/presentation/views/home/news_view/news.dart
+++ lib/presentation/views/home/news_view/news.dart
@@ -13,12 +13,11 @@ final localizedNews = {
         color: CustomTheme.primaryColor,
       ),
     ),
-
     NewsItem(
       title: 'Auswertung',
-      text: '',
+      text: 'Es gibt jetzt Auswertungen für jedes Spiel. Sie zeigt dir pro Spieler:in verschiedene Metriken an, mit welchen Ihr euch untereinander vergleichen könnt. Danke an Cameron für diesen Vorschlag!',
       icon: const SFIcon(
-        SFIcons.sf_graph_2d,
+        SFIcons.sf_chart_bar_fill,
         fontSize: 28,
         color: CustomTheme.primaryColor,
       ),
@@ -43,7 +42,7 @@ final localizedNews = {
     ),
     NewsItem(
       title: 'Problem mit Kamikaze behoben',
-      text: 'Kamikaze addiert nun immer die Hälfte vom Punktelimit auf die Punktestände. Vorher wurde immer 50 Punkte addiert.',
+      text: 'Kamikaze addiert nun immer die Hälfte vom Punktelimit auf die Punktestände. Vorher wurde immer 50 Punkte addiert. Danke an Justus für den Hinweis!',
       icon: const SFIcon(
         SFIcons.sf_wrench_adjustable_fill,
         fontSize: 28,
@@ -63,9 +62,9 @@ final localizedNews = {
     ),
     NewsItem(
       title: 'Evaluation',
-      text: '',
+      text: 'There are now statistics available for every game. They show you various metrics for each player, which you can use to compare yourselves with one another. Thanks to Cameron for this suggestion!',
       icon: const SFIcon(
-        SFIcons.sf_graph_2d,
+        SFIcons.sf_chart_bar_fill,
         fontSize: 28,
         color: CustomTheme.primaryColor,
       ),
@@ -91,7 +90,7 @@ final localizedNews = {
 
     NewsItem(
       title: 'Kamikaze Issue Fixed',
-      text: 'Kamikaze now always adds half of the point limit to the scores. Previously it always added 50 points.',
+      text: 'Kamikaze now always adds half of the point limit to the scores. Previously it always added 50 points. Thanks to Justus for the hint!',
       icon: const SFIcon(
         SFIcons.sf_wrench_adjustable_fill,
         fontSize: 28,
