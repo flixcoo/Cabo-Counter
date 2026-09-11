c85f7849db62e90ab6d9de296aa0b587ac099aed
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-09-11 10:09:07 +0200

    Removed new view padding

--- lib/presentation/views/home/news_view/news_view.dart
+++ lib/presentation/views/home/news_view/news_view.dart
@@ -13,68 +13,73 @@ class NewsView extends StatelessWidget {
   Widget build(BuildContext context) {
     final loc = AppLocalizations.of(context);
     final locale = loc.localeName;
-    final items = localizedNews.containsKey(locale)
+    final newsItems = localizedNews.containsKey(locale)
         ? localizedNews[locale]
         : localizedNews['en'];
 
-    return Dialog.fullscreen(
+    return Scaffold(
       backgroundColor: CustomTheme.backgroundColor,
-      child: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 50),
-                child: SingleChildScrollView(
-                  child: Column(
-                    spacing: 40,
+      body: SafeArea(
+        child: Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                spacing: 40,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Invisible element to have some spacing on top
+                  const SizedBox.shrink(),
+
+                  // Texts
+                  Center(
+                    child: Column(
+                      children: [
+                        // title
+                        Text(
+                          loc.whats_new,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: CustomTheme.textColor,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold,
+                            height: 1.3,
+                          ),
+                        ),
+
+                        // Version
+                        Text(
+                          '${loc.version} ${VersionService.getVersionNumber()}',
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: CustomTheme.textColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Items
+                  Column(
+                    spacing: 20,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Center(
-                        child: Column(
-                          children: [
-                            Text(
-                              loc.whats_new,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                color: CustomTheme.textColor,
-                                fontSize: 28,
-                                fontWeight: FontWeight.bold,
-                                height: 1.3,
-                              ),
-                            ),
-                            Text(
-                              '${loc.version} ${VersionService.getVersionNumber()}',
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                color: CustomTheme.textColor,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Column(
-                        spacing: 20,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          // News Items
-                          for (final item in items ?? [])
-                            NewsTile(newsItem: item),
-                        ],
-                      ),
-
-                      // Continue button
-                      FloatingAnimatedButton(
-                        onPressed: () => Navigator.pop(context),
-                        text: loc.continu,
-                      ),
+                      // News Items
+                      for (final item in newsItems ?? [])
+                        NewsTile(newsItem: item),
                     ],
                   ),
-                ),
+
+                  // Continue button
+                  FloatingAnimatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    text: loc.continu,
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
